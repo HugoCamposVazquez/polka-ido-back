360d5cf693be4bea91885c0fd496a131bc214373
tcar <tcar121293@gmail.com>
2021-08-20 09:23:00 +0200

    add dummy test;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- test/e2e/sample.spec.ts
+++ test/e2e/sample.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 
-describe("Sample e2e test", function () {
-    it("Should successfully query database", async function () {
+describe("dummy e2e test", function () {
+    it("tests nothing", async function () {
         expect(true).to.be.deep.equal(true);
     });
   });
