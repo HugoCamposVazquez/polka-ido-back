ae8264c6428777638a52e3de3ad1bb3aa4d3aa22
tcar <tcar121293@gmail.com>
2021-08-20 09:22:15 +0200

    add dummy tests;
    Signed-off-by: tcar <tcar121293@gmail.com>

---
+++ test/e2e/sample.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from "chai";
+
+describe("Sample e2e test", function () {
+    it("Should successfully query database", async function () {
+        expect(true).to.be.deep.equal(true);
+    });
+  });
---
+++ test/unit/controller/sample.spec.ts
@@ -0,0 +1,6 @@
+import { expect } from "chai";
+describe("dummy test", function () {
+    it("tests nothing", async function () {
+        expect(true).to.be.deep.equal(true);
+    });
+  });
