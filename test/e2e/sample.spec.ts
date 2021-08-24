631f05f53349ccf1095d7ef708400cb59c699b96
tcar <tcar121293@gmail.com>
2021-08-24 16:09:14 +0200

    add dummy tests;
    Signed-off-by: tcar <tcar121293@gmail.com>

--- test/e2e/sample.spec.ts
+++ test/e2e/sample.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from "chai";
 
 describe("dummy e2e test", function () {
-    it("tests nothing", async function () {
-        expect(true).to.be.deep.equal(true);
-    });
+  it("tests nothing", async function () {
+    expect(true).to.be.deep.equal(true);
   });
+});
--- test/unit/controller/sample.spec.ts
+++ test/unit/controller/sample.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 describe("dummy test", function () {
-    it("tests nothing", async function () {
-        expect(true).to.be.deep.equal(true);
-    });
+  it("tests nothing", async function () {
+    expect(true).to.be.deep.equal(true);
   });
+});
